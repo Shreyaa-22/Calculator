9eb9e1dbe5599a977b3945da441f96e9e1ec1f5c
Shreyaa-22 <72153815+Shreyaa-22@users.noreply.github.com>
2021-06-26 14:48:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
+var Arr = [];
+var count = 0;
+
 function hideElement() {
     document.getElementById("dis").value = "";
 }
 
 function display(val) {
     document.getElementById("dis").value += val;
+    Arr.push(document.getElementById("dis").value);
+    count++;
 }
 
 function calc() {
     try {
         var a = document.getElementById("dis").value;
-        console.log(typeof (a));
         var ans = eval(a);
         document.getElementById("dis").value = ans;
+        Arr.push(ans);
+        count++;
     } catch (e) {
         document.getElementById("dis").value = "INVALID INPUT";
         setTimeout(hideElement, 500);
+        Arr = [];
+        count = 0;
     }
 }
 
 function clr() {
     document.getElementById("dis").value = "";
+    Arr = [];
+    count = 0;
 }
 
 function rem() {
-    var a = document.getElementById("dis").value;
-    var cleared = a.slice(0, -1);
-    document.getElementById("dis").value = cleared;
+    if (count == 0) {
+        document.getElementById("dis").value = "";
+    } else {
+        Arr.pop();
+        count--;
+        if (count == 0) document.getElementById("dis").value = "";
+        else document.getElementById("dis").value = Arr[count - 1];
+
+    }
 }
